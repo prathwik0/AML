51471f7230ff3241e4245d2b723e282293234228
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-06-28 11:57:34 +0530

    Create spider paragraph.py

---
+++ scrapy/scraper/scraper/spiders/paragraph.py
@@ -0,0 +1,18 @@
+import scrapy
+
+
+class ParagraphSpider(scrapy.Spider):
+    name = "paragraph"
+    allowed_domains = ["wikipedia.org"]
+    start_urls = ["http://wikipedia.org/"]
+
+    def request(self):
+        urls = ['https://en.wikipedia.org/wiki/Titanic', 'https://en.wikipedia.org/wiki/Titanic_(1997_film)']
+        return [scrapy.Request(url = url, callback = self.parse) for url in urls]
+
+    def parse(self, response):
+        url = response.url
+        paras = response.css('h1::text').getall()
+        print(paras)
+        for para in paras:
+            print('Paragraph : ', para)
