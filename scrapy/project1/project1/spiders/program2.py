fc1cf51f1bacaa38e883a6fc61c428f8bc4e5b0b
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-07-23 10:07:22 +0530

    Create program2.py

---
+++ scrapy/project1/project1/spiders/program2.py
@@ -0,0 +1,18 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+
+class MySpider(CrawlSpider):
+    name = "program2"
+    allowed_domains = ["google.com"]
+    start_urls = ["https://www.google.com/"]
+    rules = (Rule(LinkExtractor(), callback="parse_page", follow=True),)
+
+    def parse_page(self, response):
+        title = response.css("title::text").get()
+        paragraphs = response.css("p::text").getall()[:2]
+        yield {
+            "title": title,
+            "paragraphs": paragraphs,
+        }
